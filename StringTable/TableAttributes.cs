3f3f86ea0ee4c79b9a887365c11cfc50b370e2c2
jul1u5 <marozas.julius@gmail.com>
2018-10-12 21:12:28 +0300

    Added alignment enum and properties

--- StringTable/TableAttributes.cs
+++ StringTable/TableAttributes.cs
@@ -1,8 +1,17 @@
 namespace StringTable
 {
+    public enum Alignment
+    {
+        Right,
+        Center,
+        Left
+    }
+
     [System.AttributeUsage(System.AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
     public sealed class TableAttribute : System.Attribute
     {
+        public Alignment HeadAlign { get; set; } = Alignment.Center;
+        public Alignment BodyAlign { get; set; } = Alignment.Right;
         public bool ExcludeByDefault { get; set; }
     }
 
@@ -13,6 +22,9 @@ namespace StringTable
                            AllowMultiple = false)]
     public sealed class TableColumnAttribute : System.Attribute
     {
+        public Alignment HeadAlign { get; set; }
+        public Alignment BodyAlign { get; set; }
+
         public int Order { get; set; }
         public bool Include { get; set; }
         public bool Exclude { get; set; }
