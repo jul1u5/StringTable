d7aa9ba1198dddec7884d9ddf579873e0541c1f2
jul1u5 <marozas.julius@gmail.com>
2018-10-14 14:24:26 +0300

    Made attributes public

--- StringTable/TableAttributes.cs
+++ StringTable/TableAttributes.cs
@@ -1,7 +1,7 @@
 namespace StringTable
 {
     [System.AttributeUsage(System.AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
-    sealed class TableAttribute : System.Attribute
+    public sealed class TableAttribute : System.Attribute
     {
         public bool ExcludeByDefault { get; set; }
     }
@@ -11,7 +11,7 @@ namespace StringTable
                            System.AttributeTargets.Property,
                            Inherited = true,
                            AllowMultiple = false)]
-    sealed class TableColumnAttribute : System.Attribute
+    public sealed class TableColumnAttribute : System.Attribute
     {
         public int Order { get; set; }
         public bool Include { get; set; }
